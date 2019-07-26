caf31b4c0d6eab7bf117e8f87a035423bf598a6e
Lauri Eskola <lauri.eskola@acquia.com>
2019-07-26 21:41:49 +0300

    Initial demo of react-select

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Creatable from 'react-select/creatable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const components = {
+  DropdownIndicator: null,
+};
+
+class App extends React.Component {
+  state = {
+    selectedOption: null,
+    isFetching: null,
+    options: null,
+    inputValue: '',
+  };
+
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.selectRef = React.createRef();
+  }
+
+  fetchPosts = () => {
+    this.setState({ isFetching: true });
+    fetch('https://jsonplaceholder.typicode.com/posts').then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      const options = result.map((post) => {
+        return { value: post.id, label: post.title };
+      });
+      this.setState({ options: options });
+    });
+  };
+
+  handleChange = selectedOption => {
+    this.setState({ ...this.state, selectedOption }, () => {
+      this.selectRef.current.focus();
+    });
+    console.log(`Option selected:`, selectedOption);
+  };
+
+  handleInputChange(newValue) {
+    if (!newValue) {
+      this.setState({ options: null });
+    } else {
+      this.fetchPosts();
+    }
+    this.setState( { inputValue: newValue });
+  }
+
+  render() {
+    const { selectedOption, options, inputValue } = this.state;
+
+    return (
+      <div className="app">
+        <Creatable
+          inputValue={inputValue}
+          components={components}
+          value={selectedOption}
+          options={options}
+          onChange={this.handleChange}
+          onInputChange={this.handleInputChange}
+          isMulti
+          ref={this.selectRef}
+          menuIsOpen={options !== null ? undefined : false}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
